2bfa42da473a1207683b620032a497b4001556c7
Anders von Mirbach <121372433+Ders-vm@users.noreply.github.com>
2024-01-16 12:58:17 -0700

    Test

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
     <main>
-    <p>Hello World!</p>
+    <h1 className="text4xl">Hello World!</h1>
+    <p className="text2xl">This is a Next.js app with Tailwind CSS</p>
+    
+    
+
     </main>
   );
 }
